c80a18698214a17c4f149852b35e0aac89dae284
2019027001 <artichoke42@naver.com>
2021-11-30 17:08:06 +0900

    fix buf

--- 12_error_handling/02_error_type.rs
+++ 12_error_handling/02_error_type.rs
@@ -3,17 +3,9 @@ use std::num::ParseIntError;
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty = item_quantity.parse::<i32>()?;
     
-    match qty{
-        Ok(cnt) => Ok(cnt * cost_per_item + processing_fee),
-        Err(error) => match error.kind(){
-            ParseIntError => Err("invalid digit found in string"),
-            other => {
-                panic!("Parse Error")
-            }
-        }
-    }
+    Ok(qty * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]
