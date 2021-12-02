6707edceb1b762ba06368bb6267477945f095f4e
2019027001 <artichoke42@naver.com>
2021-12-02 20:50:53 +0900

    add options answer(?) code

---
+++ 14_options/02_if_let.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let optional_word = Some(String::from("rustlings"));
+    if let Some(word) = optional_word {
+        println!("The word is: {}", word);
+    } else {
+        println!("The optional word doesn't contain anything");
+    }
+
+    let mut optional_integers_vec: Vec<Option<i8>> = Vec::new();
+    for x in 1..10 {
+        optional_integers_vec.push(Some(x));
+    }
+
+    while let Some(Some(integer)) = optional_integers_vec.pop() {
+        println!("current value: {}", integer);
+    }
+}
