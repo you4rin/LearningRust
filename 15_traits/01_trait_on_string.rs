018c5b5e56829ff5044ec671ab0f187a1fc154be
2019027001 <artichoke42@naver.com>
2021-12-02 21:16:24 +0900

    add traits part answer code

---
+++ 15_traits/01_trait_on_string.rs
@@ -0,0 +1,34 @@
+trait AppendBar {
+    fn append_bar(self) -> Self;
+}
+
+impl AppendBar for String {
+    fn append_bar(mut self) -> String{
+        self.push_str("Bar");
+        self
+    }
+}
+
+fn main() {
+    let s = String::from("Foo");
+    let s = s.append_bar();
+    println!("s: {}", s);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_foo_bar() {
+        assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
+    }
+
+    #[test]
+    fn is_bar_bar() {
+        assert_eq!(
+            String::from("").append_bar().append_bar(),
+            String::from("BarBar")
+        );
+    }
+}
