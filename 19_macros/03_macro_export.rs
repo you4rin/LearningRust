a8d93e69f9b0dfb0d205dbd3d1e7b59986a98a42
2019027001 <artichoke42@naver.com>
2022-01-23 15:40:51 +0900

    add macros solution code

---
+++ 19_macros/03_macro_export.rs
@@ -0,0 +1,12 @@
+mod macros {
+    #[macro_export]
+    macro_rules! my_macro {
+        () => {
+            println!("Check out my macro!");
+        };
+    }
+}
+
+fn main() {
+    my_macro!();
+}
